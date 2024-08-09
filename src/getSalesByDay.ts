31f6bbb6384ee8b0c7e3c454289bd99a071a14d4
bel-goncalves <igl@cesar.school>
2024-08-09 17:03:33 -0300

    getMostSales

---
+++ src/getSalesByDay.ts
@@ -0,0 +1,61 @@
+import { parse, getDay } from "date-fns";
+import { ptBR } from "date-fns/locale";
+import { Purchase } from "./interfaces";
+
+const daysCount: { [key: string]: number } = {
+  Domingo: 0,
+  "Segunda-feira": 0,
+  "Terça-feira": 0,
+  "Quarta-feira": 0,
+  "Quinta-feira": 0,
+  "Sexta-feira": 0,
+  Sábado: 0,
+};
+
+const daysNames = [
+  "Domingo",
+  "Segunda-feira",
+  "Terça-feira",
+  "Quarta-feira",
+  "Quinta-feira",
+  "Sexta-feira",
+  "Sábado",
+];
+
+export default function getSalesByDay(data: Purchase[]) {
+  data.forEach((value) => {
+    const formatDate = parse(value.Data, "dd/MM/yyyy HH:mm", new Date(), {
+      locale: ptBR,
+    });
+
+    const weekDay = getDay(formatDate);
+    const weekDayName = daysNames[weekDay];
+    daysCount[weekDayName]++;
+
+    return daysCount;
+  });
+
+  function getMostSales(daysCount: { [key: string]: number }) {
+    let numSales = 0;
+    let mostSales = "";
+
+    for (const day in daysCount) {
+      if (daysCount[day] > numSales) {
+        numSales = daysCount[day];
+        mostSales = day;
+      }
+    }
+    showSalesByDay(mostSales);
+  }
+
+  getMostSales(daysCount);
+}
+
+function showSalesByDay(mostSales: string) {
+  const status = document.getElementById("status");
+  if (status) {
+    status.innerHTML += `
+            <li>Dia com mais vendas: ${mostSales}</li>
+        `;
+  }
+}
--- src/script.ts
+++ src/script.ts
@@ -1,58 +1,19 @@
 import fetchData from "./fetchData.js";
 import getPayMethods from "./getPayMethods.js";
+import getSalesByDay from "./getSalesByDay.js";
 import getStatus from "./getStatus.js";
 import { Purchase } from "./interfaces.js";
 import sumValues from "./sumValues.js";
-import { parse, getDay } from "date-fns";
-import { ptBR } from "date-fns/locale";
 
 async function handleData() {
   const data = await fetchData<Purchase[]>(
     "https://api.origamid.dev/json/transacoes.json"
   );
   if (data) {
-    console.log(data);
-
-    const daysCount: { [key: string]: number } = {
-      Domingo: 0,
-      "Segunda-feira": 0,
-      "Terca-feira": 0,
-      "Quarta-feira": 0,
-      "Quinta-feira": 0,
-      "Sexta-feira": 0,
-      Sábado: 0,
-    };
-
-    const daysNames = [
-      "Domingo",
-      "Segunda-feira",
-      "Terca-feira",
-      "Quarta-feira",
-      "Quinta-feira",
-      "Sexta-feira",
-      "Sábado",
-    ];
-
-    function getSalesByDay() {
-      data?.forEach((value) => {
-        const formatDate = parse(value.Data, "dd/MM/yyyy HH:mm", new Date(), {
-          locale: ptBR,
-        });
-
-        const weekDay = getDay(formatDate);
-        const weekDayName = daysNames[weekDay];
-        daysCount[weekDayName]++;
-
-        return daysCount;
-      });
-    }
-    getSalesByDay();
-
-    console.log(daysCount); //ok
-
     sumValues(data);
     getPayMethods(data);
     getStatus(data);
+    getSalesByDay(data);
   }
 }
 handleData();
